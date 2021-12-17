2b9f5adfc1321b17b4e7f350b395702b307a29ee
Felipe Pardo <felipepardo90@hotmail.com>
2021-12-17 11:50:20 -0300

    primer desafío Javascript - algoritmo

---
+++ main.js
@@ -0,0 +1,17 @@
+let nombre = prompt("Ingresa tu nombre")
+let apellido = prompt("Ingresa tu apellido")
+
+let num1 = parseInt(prompt("Ingresa un número para sumar"))
+let num2 = parseInt(prompt("Ingresa otro número"))
+
+let texto1 = "FELIPE"
+let texto2 = "PARDO"
+
+alumnoNombre = (nombre + " " + apellido)
+mensajeTexto = (texto1 + texto2)
+
+suma = (num1 + num2)
+
+alert("Hola " + mensajeTexto)
+console.log("¡¡¡Hola " + alumnoNombre + "!!!")
+console.log(suma)
