c54ad721f3d2ea30c667646e2fe76ea243357db9
Felipe Pardo <felipepardo90@hotmail.com>
2021-12-30 22:59:53 -0300

    Calculadora simple

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// let numero1 = parseInt(prompt("Ingresa un número"))
+// let operador = prompt("Escoge un operador (+, -, * o /)")
+// let numero2 = parseInt(prompt("Ingresa otro número"))
+
+// function calculadora(numero1, operador, numero2) {
+//     switch (operador) {
+//         case "+":
+//             return numero1 + numero2
+//             break
+//         case "-":
+//             return numero1 - numero2
+//             break
+//         case "/":
+//             return numero1 / numero2
+//             break
+//         case "*":
+//             return numero1 * numero2
+//             break
+//         default:
+//             return 0
+//             break
+//     }
+// }
+
+// document.write(calculadora(numero1, operador, numero2))
