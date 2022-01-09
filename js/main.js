179b45ffcefe00ebcd183bb3a6fc59ece0b1f74f
Felipe Pardo <felipepardo90@hotmail.com>
2022-01-08 21:55:35 -0300

    Añadiendo objeto y constructor

--- js/main.js
+++ js/main.js
@@ -1,28 +1,48 @@
-function solicitarPréstamos() {
+// function solicitarPréstamos() {
 
-    if (montoInicial > 50000) {
-        alert("No están aprobados préstamos mayores a 50.000")
-    } else if (montoInicial < 10000) {
-        alert("No están aprobados préstamos menores a 10.000")
-    } else {
-        alert("Su préstamo personal fue aprobado")
+//     if (montoInicial > 50000) {
+//         alert("No están aprobados préstamos mayores a 50.000")
+//     } else if (montoInicial < 10000) {
+//         alert("No están aprobados préstamos menores a 10.000")
+//     } else {
+//         alert("Su préstamo personal fue aprobado")
+//     }
+// }
+
+
+// function sumarIva(montoInicial, iva) {
+//     let montoFinal = montoInicial * iva
+//     return montoFinal
+// }
+
+
+// function valorFinal() {
+//     alert(`El cálculo final de su préstamo con IVA es de ${montoFinal}`)
+// }
+
+// let montoInicial = parseInt(prompt("Ingresa un monto"))
+// const iva = 1.215 //TNA 21.5
+// let montoFinal = sumarIva(montoInicial, iva)
+// solicitarPréstamos()
+// sumarIva(montoInicial, iva)
+// valorFinal()
+
+
+function capturar() {
+
+    function Prestamo(monto, cuotas) {
+
+        this.monto = monto
+        this.cuotas = cuotas
+        alert(`Vas a solicitar un préstamo de ${monto} en ${cuotas} cuotas. ¿Deseas continuar?`)
+
     }
+
+
+    let seleccionarMonto = document.getElementById("monto").value
+    let seleccionarCuotas = document.getElementById("cuotas").value
+
+    console.log(seleccionarMonto, seleccionarCuotas)
+
+    let nuevoPrestamo = new Prestamo(seleccionarMonto, seleccionarCuotas)
 }
-
-
-function sumarIva(montoInicial, iva) {
-    let montoFinal = montoInicial * iva
-    return montoFinal
-}
-
-
-function valorFinal() {
-    alert(`El cálculo final de su préstamo con IVA es de ${montoFinal}`)
-}
-
-let montoInicial = parseInt(prompt("Ingresa un monto"))
-const iva = 1.215 //TNA 21.5
-let montoFinal = sumarIva(montoInicial, iva)
-solicitarPréstamos()
-sumarIva(montoInicial, iva)
-valorFinal()
