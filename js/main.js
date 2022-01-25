d38240ce5e4dd43e6cda3b8aed1c11e08f6f3161
Felipe Pardo <felipepardo90@hotmail.com>
2022-01-25 19:40:41 -0300

    addEventListener to ID boton -- evento clase 9

--- js/main.js
+++ js/main.js
@@ -1,32 +1,3 @@
-// function solicitarPréstamos() {
-
-//     if (montoInicial > 50000) {
-//         alert("No están aprobados préstamos mayores a 50.000")
-//     } else if (montoInicial < 10000) {
-//         alert("No están aprobados préstamos menores a 10.000")
-//     } else {
-//         alert("Su préstamo personal fue aprobado")
-//     }
-// }
-
-
-// function sumarIva(montoInicial, iva) {
-//     let montoFinal = montoInicial * iva
-//     return montoFinal
-// }
-
-
-// function valorFinal() {
-//     alert(`El cálculo final de su préstamo con IVA es de ${montoFinal}`)
-// }
-
-// let montoInicial = parseInt(prompt("Ingresa un monto"))
-// const iva = 1.215 //TNA 21.5
-// let montoFinal = sumarIva(montoInicial, iva)
-// solicitarPréstamos()
-// sumarIva(montoInicial, iva)
-// valorFinal()
-
 class Prestamo {
     constructor(monto, cuotas) {
 
@@ -46,6 +17,19 @@ class Prestamo {
 }
 
 
+let tablaPrestamo = []
+
+
+const boton = document.getElementById("boton")
+
+boton.addEventListener("click", function agregar() {
+    tablaPrestamo.push(nuevoPrestamo)
+    console.log(tablaPrestamo)
+    document.getElementById("tabla").innerHTML += `<tbody><td>${nuevoPrestamo.monto}</td><td>${nuevoPrestamo.cuotas}</td><td>${nuevoPrestamo.valorCuotas}</td></tbody>`
+    alert(`Solicitaste un préstamo por ${nuevoPrestamo.monto} a pagar en ${nuevoPrestamo.cuotas} cuotas. El valor final de cada cuota será de ${nuevoPrestamo.valorCuotas}. ¿Desea continuar? `)
+})
+
+
 function calcular() {
 
     const seleccionarMonto = document.getElementById("monto").value
@@ -62,31 +46,3 @@ function calcular() {
 
     agregar()
 }
-
-let tablaPrestamo = []
-
-function agregar() {
-    tablaPrestamo.push(nuevoPrestamo)
-    console.log(tablaPrestamo)
-    document.getElementById("tabla").innerHTML += `<tbody><td>${nuevoPrestamo.monto}</td><td>${nuevoPrestamo.cuotas}</td><td>${nuevoPrestamo.valorCuotas}</td></tbody>`
-    alert(`Solicitaste un préstamo por ${nuevoPrestamo.monto} a pagar en ${nuevoPrestamo.cuotas} cuotas. El valor final de cada cuota será de ${nuevoPrestamo.valorCuotas}. ¿Desea continuar? `)
-}
-
-// Array creado para desafío --- préstamos más solicitados
-
-let mostRequested = [
-    { monto: 75.000, cuotas: 24, final: 5.312 }, { monto: 20.000, cuotas: 12, final: 2.833 }, { monto: 90.000, cuotas: 36, final: 4.250 }, { monto: 50.000, cuotas: 24, final: 3.541 },
-
-]
-
-console.log(mostRequested)
-
-mostRequested.sort((a, b) => {
-    if (a.final > b.monfinalto) {
-        return 1
-    }
-    if (a.final < b.final) {
-        return -1
-    }
-    return 0
-})
