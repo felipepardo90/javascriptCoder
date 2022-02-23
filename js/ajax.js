3405664bbc4cbc89be8dc1f87e28e2752bc2433c
Felipe Pardo <felipepardo90@hotmail.com>
2022-02-22 23:23:51 -0300

    Conectando a API dólar

--- js/ajax.js
+++ js/ajax.js
@@ -1,32 +1,38 @@
-// desafío AJAX
 
-document.querySelector('#btnJson').addEventListener('click', comentarios)
+function fetchData() {
+  fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
 
-function comentarios() {
-
-    const comment = new XMLHttpRequest()
-    comment.open('GET', 'https://jsonplaceholder.typicode.com/posts/1/comments', true)
-
-    comment.send()
-
-    comment.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            let parseComment = JSON.parse(this.responseText)
-
-            let respuesta = document.querySelector('#respuesta')
-            respuesta.innerHTML = ''
-
-            for (let item of parseComment) {
-                respuesta.innerHTML += `
-                <h6>${item.name}</h6>
-                <h6>${item.email}</h6>
-                <p>${item.body}</p>
-                `
-            }
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      const filteredOutput = data.filter(item => {
+        switch (item.casa.nombre) {
+          case "Dolar Blue":
+          case "Dolar Oficial":
+              case "Dolar turista":
+                case "Dolar Contado con Liqui":
+                    case "Dolar":
+            return item;
+            break;
+          default:
+            return null;
         }
-    }
+      })
+      let html = "";
+      filteredOutput.forEach(item => {
+        html += "<div class=\"user container\">";
+        html += `<p>${item.casa.nombre}</p>`;
+        html += `<p>Compra: ${item.casa.compra}</p>`;
+        html += `<p>Venta: ${item.casa.venta}</p>`;
+        html += `<p>Variación: ${item.casa.variacion}</p>`;
+        html += "</div>";
+      })
+
+      document
+        .querySelector('#app')
+        .insertAdjacentHTML("afterbegin", html);
+    });
 }
 
-// El Ejemplo está hecho con un doc JSON de JSON PLACEHOLDER
-
-// PARA VER COMENTARIOS, PRESIONAR EN VER COMENTARIOS
+fetchData();
