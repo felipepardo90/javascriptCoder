ac9f31167c5449447711073b1e369f4eb5789b4c
Felipe Pardo <fpardo@UTESALTA.NET>
2022-10-05 15:42:57 -0300

    funcionalidades

--- js/main.js
+++ js/main.js
@@ -73,7 +73,7 @@ botonTabla.addEventListener("click", () => {
   tablaPrestamo.push(nuevoPrestamo);
   document.getElementById(
     "tabla"
-  ).innerHTML += `<tbody><td>${nuevoPrestamo.nombre}</td><td>${nuevoPrestamo.apellido}</td><td>${nuevoPrestamo.dni}</td><td>${nuevoPrestamo.monto}</td><td>${nuevoPrestamo.cuotas}</td><td>${nuevoPrestamo.valorCuotas}</td></tbody>`;
+  ).innerHTML += `<tbody><td>${nuevoPrestamo.nombre} ${nuevoPrestamo.apellido}</td><td>${nuevoPrestamo.dni}</td><td>${nuevoPrestamo.monto}</td><td>${nuevoPrestamo.cuotas}</td><td>${nuevoPrestamo.valorCuotas}</td></tbody>`;
   // alert(`Solicitaste un préstamo por ${nuevoPrestamo.monto} a pagar en ${nuevoPrestamo.cuotas} cuotas. El valor final de cada cuota será de ${nuevoPrestamo.valorCuotas}. ¿Desea continuar? `)
 
   localStorageTablaPrestamo(tablaPrestamo);
